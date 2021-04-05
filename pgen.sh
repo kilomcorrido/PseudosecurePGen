e92f7f1162cf65b9655fe095371da06983ae13ac
kilomcorrido <53127617+kilomcorrido@users.noreply.github.com>
2021-04-05 10:21:11 -0600

    Added salt functionality and added 1337ness
    Added salting and changed the Caesar cipher to be way more cool

--- pgen.sh
+++ pgen.sh
@@ -1,41 +1,40 @@
 #!/bin/bash
 
-#Create variable of the first 150 bytes of /dev/urandom output
+#Enter your salt
+read -p "Enter a random string to be your salt. Be sure to get real freaky with it! $(echo "")" salt
+
+#Take first 150 bytes of /dev/urandom output
 randomness=$(cat /dev/urandom | head -c 150)
-
-#Create variable using temperature anisotropies of the Cosmic Microwave Background from NASA's website
+#Take most recent temperature anisotropies of the cosmic microwave background from NASA website, append to same file
 background=$(curl https://lambda.gsfc.nasa.gov/data/suborbital/ACT/act_v2/TotalSpectra/spectrum_ACTEplusS_148x148.dat)
-
-#Calculate the first 1000 digits of Pi
+#Take first 1000 digits of pi
 pi=$(echo "scale=1000; 4*a(1)" | bc -l)
 
-#Function collates these three variables into a text file, then calculates the sha512 checksum of the file and directs the result to a new file named firstsum.txt
+#Calculate sha512 checksum of all those things concatenated into tmp.txt
 function passw0rd {
-	echo $randomness > tmp.txt
-	echo $background >> tmp.txt
-	echo $pi >> tmp.txt
-	echo "$(sha512sum tmp.txt)" > firstsum.txt
+        echo $randomness > tmp.txt
+        echo $background >> tmp.txt
+        echo $pi >> tmp.txt
+        echo "$(sha512sum tmp.txt)" > firstsum.txt
 }
 
-#Function collates the three variables again, calculates a new sha512 checksum, directs the output to a new file named secondsum.txt
+#Calculate another sha512 checksum of same
 function passw2rd {
-	echo $randomness > tmp2.txt
-	echo $background >> tmp2.txt
-	echo $pi >> tmp2.txt
-	echo "$(sha512sum tmp2.txt)" > secondsum.txt
+        echo $randomness > tmp2.txt
+        echo $background >> tmp2.txt
+        echo $pi >> tmp2.txt
+        echo "$(sha512sum tmp2.txt)" > secondsum.txt
 }
 
 #Function calls
 passw0rd
 passw2rd
 
-#Concatenate first checksum and second checksum
+#Concatenate the two checksums and echo to a file
 cat firstsum.txt >> secondsum.txt
+echo "$(sha512sum secondsum.txt)" > tmp4.txt   
 
-#Create third checksum of the combined checksums and create a new file with it
-echo "$(sha512sum secondsum.txt)" > tmp4.txt
-
-#Make it look like the program is doing something
+#Make the script look like its doing something, for that USER EXPERIENCE (TM)
 echo ""
 echo -ne "."
 sleep 1
@@ -49,10 +48,9 @@ echo -ne "."
 echo -e "\n"
 echo "The following is your new password:"
 
-#Take first 25 bytes of the final checksum, reverse it, and encode with a Caesar Cipher corresponding to the alphabet backwards and all capitalized. Print to terminal as new password.
-echo "$(less tmp4.txt | head -c 25 | rev | tr abcdefghijklmnopqrstuvwxyz ZyXwVuTsRqPoNmLkJiHgFeDcBa)"
+#Here's where the magic happens: Encode with a Caesar cipher corresponding to 30 random characters taken from openssl. Base64 encode that. Then re-Caesar it with your salt.
+echo "$(less tmp4.txt | head -c 25 | rev | tr abcdefghijklmnopqrstuvwxyz $(openssl rand 30| base64) | tr abcdefghijklmnopqrstuvwxyz $salt)"
 
-#Cleanup all the temp files we fucked around with
 rm tmp.txt
 rm tmp2.txt
 rm tmp4.txt
