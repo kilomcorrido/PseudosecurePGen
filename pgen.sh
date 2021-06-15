8d6fdfd2164cd91aef8d8a8344aec8ab218ac418
kilomcorrido <53127617+kilomcorrido@users.noreply.github.com>
2021-06-15 13:05:00 -0600

    Little QoL stuff

--- pgen.sh
+++ pgen.sh
@@ -1,11 +1,27 @@
-## A pointlessly convoluted script to generate a password based on sha512 checksums and substitution ciphers. 
-## Makes you feel secure without making you actually secure! Which I think we all agree is the best kind of security.
+#Take first 150 bytes of /dev/urandom, append to tmp file
+
+#Take most recent temperature anisotropies of the cosmic microwave background from NASA #website, append to same file
+
+#Calculate first 1000 digits of pi, append to same file 
+
+#Calculate sha512sum of file and create a file of that
+
+#Do the process again to create a second file
+
+#Append the first file to the second file
+
+#Calculate the sha512sum of that
+
+#Take the first 24 bytes of it.
+#Reverse them.
+#Turn lower case to upper case and vice versa
+#Encode with a Caesar cipher corresponding to the alphabet written backwards
 
 #!/bin/bash
 
 read -p "Enter a random string to be your salt $(echo "")" salt
 
-randomness=$(cat /dev/urandom | head -c 150)
+randomness=$(cat /dev/urandom | head -c 150 > /dev/null 2>&1)
 background=$(curl -s https://lambda.gsfc.nasa.gov/data/suborbital/ACT/act_v2/TotalSpectra/spectrum_ACTEplusS_148x148.dat)
 pi=$(echo "scale=1000; 4*a(1)" | bc -l)
 
@@ -24,10 +40,11 @@ echo "$(sha512sum tmp2.txt)" > secondsum.txt
 }
 
 echo "Constructing your new password..."
+sleep 1.5
 passw0rd
 passw2rd
 cat firstsum.txt >> secondsum.txt
-echo "$(sha512sum secondsum.txt)" > tmp4.txt 
+echo "$(sha512sum secondsum.txt)" > tmp4.txt
 for ((k = 0; k <= 10 ; k++))
 do
     echo -n "[ "
@@ -39,7 +56,8 @@ do
     sleep 0.05
 done
 echo
-echo "The following is your new password:"
+sleep 0.5
+echo "Your new password is:"
 echo "$(less tmp4.txt | head -c 25 | rev | tr abcdefghijklmnopqrstuvwxyz $(openssl rand 30| base64) | tr abcdefghijklmnopqrstuvwxyz $salt | tr -dc "A-Za-z0-9^\!@%'\"#$^" </dev/urandom | head -c 25)"
 
 rm tmp.txt
